18903eb58226cc25ed9a998f1fa1f675b24e9df4
MironProFit <mpf_3d@vk.com>
2025-02-23 21:19:30 +0500

    начало работы с API

--- script.js
+++ script.js
@@ -18,10 +18,7 @@ function formatDate (date) {
     return `${day}.${month}.${year} ${hours}:${minutes}`;
 }
 
-const userComments = [
-    {name: 'Глеб Фокин', date: '12.02.22 12:18', text: 'Это будет первый комментарий на этой странице', likes: 3, like: false},
-    {name: 'Варвара Н.', date: '13.02.22 19:22', text: 'Мне нравится как оформлена эта страница! ❤', likes: 75, like: false}
-  ];
+const userComments = 
 
   function escapeHtml(text) {
     const escapeText = text
