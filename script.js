70bc117946acf47baca212944e87417f2dc6b9bb
MironProFit <mpf_3d@vk.com>
2025-03-01 13:00:20 +0500

    выполнен функционал добавления комментария через API запрос

--- script.js
+++ script.js
@@ -1,173 +1,199 @@
-const listElement = document.getElementById('list');
-const inputName = document.getElementById('input-name');
-const inputComment = document.getElementById('input-comment');
-const buttonInput = document.getElementById('button');
-const errorClass = document.querySelector('.error');
-const likeButton = document.querySelector('.like-button');
+const listElement = document.getElementById("list");
+const inputName = document.getElementById("input-name");
+const inputComment = document.getElementById("input-comment");
+const buttonInput = document.getElementById("button");
+const errorClass = document.querySelector(".error");
+const likeButton = document.querySelector(".like-button");
+
+// Форматирование выводимой даты
 
 const currentDate = new Date();
-const formattedDate = formatDate(currentDate)
+const formattedDate = formatDate(currentDate);
 
-function formatDate (date) {
-    const day = String(date.getDate()).padStart(2,'0');
-    const month = String(date.getMonth() + 1).padStart(2,'0');
-    const year = String(date.getFullYear()).slice(- 2);
-    const hours = String(date.getHours()).padStart(2,'0');
-    const minutes = String(date.getMinutes()).padStart(2,'0');
-    
-    return `${day}.${month}.${year} ${hours}:${minutes}`;
+function formatDate(date) {
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = String(date.getFullYear()).slice(-2);
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
 }
 
-const userComments = 
+// обновление массива с коментариями
 
-  function escapeHtml(text) {
-    const escapeText = text
-        .replaceAll('&', '&amp;')
-        .replaceAll('<', '&lt;')
-        .replaceAll('>', '&gt;')
-        .replaceAll('"', '&quot;')
-        .replaceAll("'", '&#39;');
-        return escapeText
-    };
+let userComments = [];
 
+const updateUserComments = (newComments) => {
+  userComments = newComments;
+};
+
+// API запрос на получение данных с сервера
+
+const host = "https://wedev-api.sky.pro/api/v1/Miron_MPF";
+
+const fetchComments = () => {
+  return fetch(host + "/comments")
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log(responseData);
+      const appComents = responseData.comments.map((comment) => {
+        return {
+          name: comment.author.name,
+          date: comment.date,
+          text: comment.text,
+          likes: comment.likes,
+          isLiked: false,
+        };
+      });
+      return appComents;
+    });
+};
+
+fetchComments().then((data) => {
+  updateUserComments(data);
+  renderComments();
+});
+
+function escapeHtml(text) {
+  const escapeText = text
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#39;");
+  return escapeText;
+}
 
 const renderComments = () => {
+  const container = document.getElementById("list");
 
-    const container = document.getElementById('list');
-
-    container.innerHTML = userComments.map((comment, index) => 
-
+  container.innerHTML = userComments
+    .map(
+      (comments, id) =>
         `<li class="comment">
           <div class="comment-header">
-            <div class="title">${escapeHtml(comment.name)}</div>
-            <div>${comment.date}</div>
+            <div class="title">${escapeHtml(comments.name)}</div>
+            <div>${comments.date}</div>
           </div>
           <div class="comment-body">
-            <div class="comment-text">${escapeHtml(comment.text)}</div>
+            <div class="comment-text">${escapeHtml(comments.text)}</div>
           </div>
           <div class="comment-footer">
             <div class="likes">
-              <span class="likes-counter">${comment.likes}</span>
-              <button class="like-button ${comment.like ? '-active-like' : ''}" data-index ="${index}"></button>
+              <span class="likes-counter">${comments.likes}</span>
+              <button class="like-button ${
+                comments.isLiked ? "-active-like" : ""
+              }" data-id ="${id}"></button>
             </div>
           </div>
         </li>`
+    )
+    .join("");
 
-    ).join("");
+  const likeButtons = document.querySelectorAll(".like-button");
 
-    const likeButtons = document.querySelectorAll('.like-button');
+  likeButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.stopPropagation();
+      const index = button.dataset.index;
+      const comment = userComments[index];
 
-    likeButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
-            event.stopPropagation();
-            const index = button.dataset.index;
-            const comment = userComments[index];
-    
-            if (comment.like) {
-    
-                comment.likes--;
-    
-            } else {
-    
-                comment.likes++
-    
-            } 
-            comment.like = !comment.like;
-    
-            renderComments();
-        })
-        
+      if (comment.isLiked) {
+        comment.likes--;
+      } else {
+        comment.likes++;
+      }
+      comment.isLiked = !comment.isLiked;
+
+      renderComments();
     });
+  });
 
-    const quoteComment = document.getElementById('list');
+  const addClickEventToComments = () => {
+    const commentElements = document.querySelectorAll(".comment");
+    commentElements.forEach((commentElement, index) => {
+      commentElement.addEventListener("click", () => {
+        const comment = userComments[index];
 
-    const addClickEventToComments = () => {
-        const commentElements = document.querySelectorAll('.comment');
-        commentElements.forEach((commentElement, index) => {
-            commentElement.addEventListener('click', () => {
-                const comment = userComments[index];
+        const inputComment = document.getElementById("input-comment");
+        inputComment.value = `[Цитата пользователя: "${comment.text}"]`;
 
-                
-                const inputComment = document.getElementById('input-comment');
-                inputComment.value = `[Цитата пользователя: "${comment.text}"]`;
-
-            console.log(`клик по тексту коментария: ${userComments[index].text} `);
-            })
-        })
-    }
-    addClickEventToComments();
+        console.log(`клик по тексту коментария: ${userComments[index].text} `);
+      });
+    });
+  };
+  addClickEventToComments();
 };
 
 renderComments();
 
+buttonInput.addEventListener("click", () => {
+  const list = document.createElement("li");
 
-buttonInput.addEventListener('click', () => {
+  list.classList.add("comment");
 
-    const list = document.createElement('li');
+  function validateInput() {
+    let isValidation = true;
 
-    list.classList.add('comment');
-
-
-    function validateInput() {
-
-        let isValidation = true;
-
-        if (inputName.value.length <= 3) {
-
-            inputName.classList.add('error');
-            inputName.placeholder = 'Ваше имя меньше 5 символов';
-            inputName.value = '';
-            isValidation = false;
-        } else if (inputName.value.length >= 12) {
-
-            inputName.classList.add('error');
-            inputName.placeholder = 'Ваше имя больше 12 символов';
-            inputName.value = '';
-            isValidation = false;
-
-        } else {
-            
-            inputName.classList.remove('error');
-            
-        }
-
-        if (inputComment.value.length <= 5) {
-            inputComment.placeholder = 'Ваш коментарий меньше 15 символов';
-            inputComment.classList.add('error');
-            inputComment.value = '';
-            isValidation = false;
-
-        } else {
-
-            inputComment.classList.remove('error');
-        }
-        
-        return isValidation;
+    if (inputName.value.length <= 3) {
+      inputName.classList.add("error");
+      inputName.placeholder = "Ваше имя меньше 5 символов";
+      inputName.value = "";
+      isValidation = false;
+    } else if (inputName.value.length >= 12) {
+      inputName.classList.add("error");
+      inputName.placeholder = "Ваше имя больше 12 символов";
+      inputName.value = "";
+      isValidation = false;
+    } else {
+      inputName.classList.remove("error");
     }
 
-    if(!validateInput()) {
-
+    if (inputComment.value.length <= 5) {
+      inputComment.placeholder = "Ваш коментарий меньше 15 символов";
+      inputComment.classList.add("error");
+      inputComment.value = "";
+      isValidation = false;
     } else {
-       
-        const newComment = {
-          name: inputName.value, 
-          date: formattedDate, 
-          text: inputComment.value, 
-          likes: 0, 
-          like: false
-        };
+      inputComment.classList.remove("error");
+    }
 
-        userComments.push(newComment);
+    return isValidation;
+  }
 
-        renderComments();
-    } 
-    
-    inputName.value = '';
-    inputComment.value = '';
+  if (!validateInput()) {
+  } else {
+    const newComment = {
+      name: escapeHtml(inputName.value),
+      date: formatDate,
+      text: escapeHtml(inputComment.value),
+      likes: 0,
+      isLiked: false,
+    };
+  }
+
+  postComment(escapeHtml(inputComment.value), escapeHtml(inputName.value)).then(
+    () => {
+      renderComments();
+      inputName.value = "";
+      inputComment.value = "";
+    }
+  );
 });
 
-renderComments();
 
-
-
+const postComment = (text, name) => {
+  return fetch(host + "/comments", {
+    method: "POST",
+    body: JSON.stringify({
+      text,
+      name,
+    }),
+  }).then(() => {
+    return fetchComments();
+  });
+};
 
