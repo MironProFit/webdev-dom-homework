f7fd96745ace25915d4da0904d254d35f671055d
MironProFit <mpf_3d@vk.com>
2025-03-19 19:06:04 +0500

    небольшие корректировки после настройки Prettier

--- js/render.js
+++ js/render.js
@@ -1,8 +1,8 @@
-import { formatDate } from "./formatdate.js";
+import { formatDate } from './formatdate.js'
 export const renderComments = (userComments, container) => {
-  container.innerHTML = userComments
-    .map(
-      (comment, id) => `<li class="comment">
+    container.innerHTML = userComments
+        .map(
+            (comment, id) => `<li class="comment">
         <div class="comment-header">
           <div class="title">${escapeHtml(comment.name)}</div>
           <div>${formatDate(comment.date)}</div>
@@ -13,49 +13,52 @@ export const renderComments = (userComments, container) => {
         <div class="comment-footer">
           <div class="likes">
             <span class="likes-counter">${comment.likes}</span>
-            <button class="like-button ${comment.isLiked ? "-active-like" : ""}" data-id="${id}"></button>
+            <button class="like-button ${
+                comment.isLiked ? '-active-like' : ''
+            }" data-id="${id}"></button>
           </div>
         </div>
       </li>`
-    )
-    .join("");
+        )
+        .join('')
 
-  addLikeButtonListeners(userComments);
-  addClickEventToComments(userComments);
-};
+    addLikeButtonListeners(userComments)
+    addClickEventToComments(userComments)
+}
 
 const escapeHtml = (text) => {
-  return text
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#39;");
-};
+    return text
+        .replaceAll('&', '&amp;')
+        .replaceAll('<', '&lt;')
+        .replaceAll('>', '&gt;')
+        .replaceAll('"', '&quot;')
+        .replaceAll("'", '&#39;')
+}
 
 const addLikeButtonListeners = (userComments) => {
-  const likeButtons = document.querySelectorAll(".like-button");
-  likeButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-      event.stopPropagation();
-      const index = button.dataset.id;
-      const comment = userComments[index];
+    const likeButtons = document.querySelectorAll('.like-button')
+    likeButtons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            event.stopPropagation()
+            const index = button.dataset.id
+            const comment = userComments[index]
 
-      comment.isLiked ? comment.likes-- : comment.likes++;
-      comment.isLiked = !comment.isLiked;
+            comment.isLiked ? comment.likes-- : comment.likes++
+            comment.isLiked = !comment.isLiked
 
-      renderComments(userComments, document.getElementById("list"));
-    });
-  });
-};
+            renderComments(userComments, document.getElementById('list'))
+        })
+    })
+}
 
 const addClickEventToComments = (userComments) => {
-  const commentElements = document.querySelectorAll(".comment");
-  commentElements.forEach((commentElement, index) => {
-    commentElement.addEventListener("click", () => {
-      const comment = userComments[index];
-      const inputComment = document.getElementById("input-comment");
-      inputComment.value = `[Цитата пользователя: "${comment.text}"]`;
-    });
-  });
-};
+    const commentElements = document.querySelectorAll('.comment')
+    commentElements.forEach((commentElement, index) => {
+        commentElement.addEventListener('click', () => {
+            const comment = userComments[index]
+            const inputComment = document.getElementById('input-comment')
+            inputComment.value = `[Цитата пользователя: "${comment.text}"]`
+        })
+    })
+}
+
--- js/validation.js
+++ js/validation.js
@@ -1,28 +1,28 @@
 export const validateInput = (inputName, inputComment) => {
-    let isValidation = true;
-  
+    let isValidation = true
+
     if (inputName.value.length <= 3) {
-      inputName.classList.add("error");
-      inputName.placeholder = "Ваше имя меньше 5 символов";
-      inputName.value = "";
-      isValidation = false;
+        inputName.classList.add("error")
+        inputName.placeholder = "Ваше имя меньше 5 символов"
+        inputName.value = ""
+        isValidation = false
     } else if (inputName.value.length >= 30) {
-      inputName.classList.add("error");
-      inputName.placeholder = "Ваше имя больше 12 символов";
-      inputName.value = "";
-      isValidation = false;
+        inputName.classList.add("error")
+        inputName.placeholder = "Ваше имя больше 12 символов"
+        inputName.value = ""
+        isValidation = false
     } else {
-      inputName.classList.remove("error");
+        inputName.classList.remove("error")
     }
-  
+
     if (inputComment.value.length <= 5) {
-      inputComment.placeholder = "Ваш комментарий меньше 15 символов";
-      inputComment.classList.add("error");
-      inputComment.value = "";
-      isValidation = false;
+        inputComment.placeholder = "Ваш комментарий меньше 15 символов"
+        inputComment.classList.add("error")
+        inputComment.value = ""
+        isValidation = false
     } else {
-      inputComment.classList.remove("error");
+        inputComment.classList.remove("error")
     }
-  
-    return isValidation;
-  };
+
+    return isValidation
+}
