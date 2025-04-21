318d16c1642164826f86f4d5b6211aface2ab754
MironProFit <mpf_3d@vk.com>
2025-04-21 12:08:14 +0500

    все в работе

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
+const path = require('path')
+
 module.exports = {
     entry: "./js/index.js", // Входной файл, где пишем свой код
     output: {
        filename: "main.js" // Выходной файл, который подключаем к HTML
        // Сохранится он по пути "./dist/main.js"
+       ,
+       path: path.resolve(__dirname, 'dist')
     }
  }
